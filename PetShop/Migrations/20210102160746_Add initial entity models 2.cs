67b0168b015ee78c635a7c16b01df8d4ea9a71ba
Alexandra Grecu <elena.grecu@s.unibuc.ro>
2021-01-02 18:08:49 +0200

    Add migrations.

---
+++ PetShop/Migrations/20210102160746_Add initial entity models 2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetShop.Migrations
+{
+    public partial class Addinitialentitymodels2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "weight",
+                table: "Pets",
+                newName: "Weight");
+
+            migrationBuilder.RenameColumn(
+                name: "height",
+                table: "Pets",
+                newName: "Height");
+
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "Pets",
+                newName: "Age");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Weight",
+                table: "Pets",
+                newName: "weight");
+
+            migrationBuilder.RenameColumn(
+                name: "Height",
+                table: "Pets",
+                newName: "height");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Pets",
+                newName: "age");
+        }
+    }
+}
